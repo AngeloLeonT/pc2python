a0a99330de031d19c5e62a24c0f063e669917f36
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-02-01 23:15:56 +0000

    pc2

---
+++ Estructuras iterativas/p4.py
@@ -0,0 +1,21 @@
+lista = list()
+lista
+
+alumno1 = {
+    'Matemática': 15,
+    'Lengua': 17,
+    'Química': 18
+}
+
+alumno2 = {
+    'Matemática': 17,
+    'Lengua': 14,
+    'Química': 13
+}
+
+lista.append(alumno1)
+
+lista.append(alumno2)
+
+print(lista)
+
