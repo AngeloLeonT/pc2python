5e90b5dcf9c75e332c42ab8bab4f579324a416c6
AngeloLeonT <157255387+AngeloLeonT@users.noreply.github.com>
2024-02-02 13:12:34 +0000

    pc2

--- Estructuras iterativas/p3.py
+++ Estructuras iterativas/p3.py
@@ -1,21 +1,23 @@
-numeros_pares = 0
-numeros_impares = 0
-
 # Bucle while para el ingreso de números
+# Solicitar al usuario que ingrese un número
+# Verificar si el número es par o impar
+# Si se ingresa una letra u otro carácter, salir del bucle
+# Mostrar resultados
+
+
+pares = 0
+impares = 0
+
 while True:
     try:
-        # Solicitar al usuario que ingrese un número
-        numero = int(input("Ingrese un número (o cualquier letra para detenerse): "))
+        n = int(input("Ingrese un número (o cualquier letra para detenerse): "))
         
-        # Verificar si el número es par o impar
-        if numero %2 == 0:
-            numeros_pares += 1
+        if n %2 == 0:
+            pares += 1
         else:
-            numeros_impares += 1
+            impares += 1
     except ValueError:
-        # Si se ingresa una letra u otro carácter, salir del bucle
         break
 
-# Mostrar resultados
-print(f"\nCantidad de números pares: {numeros_pares}")
-print(f"Cantidad de números impares: {numeros_impares}")
+print(f"\nCantidad de números pares: {pares}")
+print(f"Cantidad de números impares: {impares}")
